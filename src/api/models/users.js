335c83ed61eb51e352ac6cf03f3c75d54451049b
GiraMorales <gira.morales@gamil.com>
2025-01-23 13:50:01 +0100

    usuario con dato relacionado

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     rol: {
       type: String,
