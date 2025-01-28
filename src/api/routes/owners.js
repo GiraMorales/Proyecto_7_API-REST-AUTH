06b8becfe281566a145b90678607f41abebfb7d4
GiraMorales <gira.morales@gamil.com>
2025-01-28 20:05:38 +0100

    ruta para insomnia

--- src/api/routes/owners.js
+++ src/api/routes/owners.js
@@ -8,7 +8,7 @@ const {
 
 const OwnerRoutes = require('express').Router();
 
-OwnerRoutes.post('/', [isAdmin], postOwner); // Crear propietario
+OwnerRoutes.post('/:id', [isAdmin], postOwner); // Crear propietario
 OwnerRoutes.get('/', getOwners); // Obtener todos los pripietarios
 OwnerRoutes.put('/:id', [isAdmin], updateOwner); // Actualizar propietario por ID
 OwnerRoutes.delete('/:id', [isAdmin], deleteOwner); // Eliminar propietario por ID
